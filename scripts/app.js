66fed432f51ff198f561938ece0227f544f7ab04
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-23 20:35:06 -0700

    creats AJAX to search cats and use form field for cats search

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,58 @@
+
 $(document).on("ready", function(){
 
+  $("form").on("submit", function (event) {
+
+    $.ajax({
+
+        // What kind of request
+        method: "GET",
+
+        // The URL for the request
+        url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+
+        // The data to send aka query parameters
+        //data: 'json',
+        data: $("form").serialize(),
+
+        // Code to run if the request succeeds;
+        // the response is passed to the function
+        success: onSuccess,
+
+        // Code to run if the request fails; the raw request and
+        // status codes are passed to the function
+  //      error: onError
+    });
+
+    event.preventDefault();
+
+  });
+
 });
+
+//??????????
+//var formData = $(this).serialize();
+
+
+  function onSuccess(json) {
+    for (var i = 0; i < json.data.length; i++) {
+    $(".gif-gallery").append("<img src=" + (json.data[i].images.fixed_height.url) + ">");
+//    console.log(json);
+//    $("div").append("<h1>"+json.title+"</h1>");
+  }
+}
+
+//  function onError(xhr, status, errorThrown) {
+//    alert("Sorry, there was a problem!");
+//    console.log("Error: " + errorThrown);
+//    console.log("Status: " + status);
+//    console.dir(xhr);
+//  }
+
+//search url
+//http://api.giphy.com/v1/gifs/search
+//query string
+//data: $("form").serialize(),
+//should give me this:
+//cats&api_key=dc6zaTOxFJmzC
+//we'll need to limit
