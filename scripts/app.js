fdf1f85d8e676777e408a99e6706359cf11e7fba
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-23 21:09:09 -0700

    uses serialize to pass any input into the search query url for AJAX

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,21 @@ $(document).on("ready", function(){
 
   $("form").on("submit", function (event) {
 
+    event.preventDefault();
+
+    var forInput = $(this).serialize();
+
+    var newUrl = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + forInput;
+
     $.ajax({
 
-        // What kind of request
         method: "GET",
 
-        // The URL for the request
-        url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+        url: newUrl,
 
         // The data to send aka query parameters
         //data: 'json',
-        data: $("form").serialize(),
+        data: 'json',
 
         // Code to run if the request succeeds;
         // the response is passed to the function
@@ -24,35 +28,19 @@ $(document).on("ready", function(){
   //      error: onError
     });
 
-    event.preventDefault();
-
   });
 
 });
 
-//??????????
-//var formData = $(this).serialize();
-
-
   function onSuccess(json) {
     for (var i = 0; i < json.data.length; i++) {
     $(".gif-gallery").append("<img src=" + (json.data[i].images.fixed_height.url) + ">");
-//    console.log(json);
-//    $("div").append("<h1>"+json.title+"</h1>");
   }
 }
 
-//  function onError(xhr, status, errorThrown) {
-//    alert("Sorry, there was a problem!");
-//    console.log("Error: " + errorThrown);
-//    console.log("Status: " + status);
-//    console.dir(xhr);
-//  }
-
-//search url
-//http://api.giphy.com/v1/gifs/search
-//query string
-//data: $("form").serialize(),
-//should give me this:
-//cats&api_key=dc6zaTOxFJmzC
-//we'll need to limit
+  function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+  }
